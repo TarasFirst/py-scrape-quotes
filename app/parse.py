f2a3779bc3c7feb6231b03e195929d963b14a93b
TarasFirst <firsttaras@gmail.com>
2024-12-23 17:28:19 +0100

    solution without be gentle to website resources

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,4 @@
 import csv
-import logging
-# import sys
 import requests
 from dataclasses import dataclass, astuple, fields
 
@@ -16,6 +14,15 @@ class Quote:
     tags: list[str]
 
 
+QUOTES_FIELDS =[field.name for field in fields(Quote)]
+
+
+def write_fields_name_csv(output_csv_path: str) -> None:
+    with open(output_csv_path, "a", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(QUOTES_FIELDS)
+
+
 def parse_single_quote(quote: Tag) -> Quote:
     return Quote(
         text=quote.select_one(".text").text,
@@ -23,58 +30,40 @@ def parse_single_quote(quote: Tag) -> Quote:
         tags=[tag.text for tag in quote.select(".tag")],
     )
 
+def get_soup(url: str=BASE_URL) -> BeautifulSoup:
+    text = requests.get(url).content
+    return BeautifulSoup(text, features="html.parser")
 
-def get_all_quotes() -> [Quote]:
-    text = requests.get(BASE_URL).content
-    soup = BeautifulSoup(text, features="html.parser")
+
+def get_single_page_quotes(url: str=BASE_URL) -> [Quote]:
+    soup = get_soup(url)
     all_quotes = soup.select(".quote")
     return [parse_single_quote(quote) for quote in all_quotes]
 
 
-QUOTES_FIELDS =[field.name for field in fields(Quote)]
-
-def write_csv() -> None:
-    quotes = get_all_quotes()
-    with open("output_csv_path.csv", "w") as csvfile:
+def write_csv(output_csv_path: str, url) -> None:
+    quotes = get_single_page_quotes(url)
+    with open(output_csv_path, "a", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(QUOTES_FIELDS)
         writer.writerows([astuple(quote) for quote in quotes])
 
 
-# def get_single_page_quotes(page_soup: Tag) -> [Quote]:
-#     quotes = page_soup.select(".text")
-#     return [parse_single_quote(quote) for quote in quotes]
-#
-#
-# def get_url_current_page():
-#     return
-#
-#
-#     # href = "/page/1/"
-#
-#     current_page = f"{BASE_URL}page/{num_page}"
-#
-#     while BeautifulSoup(requests.get(BASE_URL).content).select_one(".next"):
-#         all_quotes.extend(get_single_page_quotes(next_page_soup))
-#     return all_quotes
-#
-#
-
-#
-#
+def parse_all_site(output_csv_path: str):
+    write_fields_name_csv(output_csv_path)
+    all_quotes = get_single_page_quotes()
+    num_page = 1
+    soup = get_soup()
+    while soup.select_one(".next"):
+        url = f"{BASE_URL}/page/{num_page}"
+        write_csv(output_csv_path, url)
+        num_page += 1
+        soup = get_soup(f"{BASE_URL}/page/{num_page}")
+    return all_quotes
 
 
-# def main(output_csv_path: str) -> None:
-#     with open(output_csv_path, "w") as csvfile:
-#         writer = csv.writer(csvfile)
-#         writer.writerow(QUOTES_FIELDS)
-#         writer.writerows([astuple(quote) for quote in QUOTES])
+def main(output_csv_path: str) -> None:
+    return parse_all_site(output_csv_path)
 
-def main():
-    # return get_all_quotes()
-    # get_all_quotes()
-    return write_csv()
 
 if __name__ == "__main__":
-    # main("quotes.csv")
-   main()
+    main("quotes.csv")
